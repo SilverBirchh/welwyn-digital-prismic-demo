9e7a1f3b17359d1144ed92519b26a72315a373cb
Bradley Birch <bradleybirch09@yahoo.co.uk>
2023-03-23 17:50:48 +0000

    fix build

--- src/prismic.ts
+++ src/prismic.ts
@@ -1,9 +1,9 @@
-import Prismic from '@prismicio/client';
+import * as prismic from "@prismicio/client";
 
 const API_ENDPOINT = import.meta.env.ASTRO_PUBLIC_PRISMIC_URL;
 if (!API_ENDPOINT) throw new Error('API_ENDPOINT is required');
 
-const client = Prismic.createClient(API_ENDPOINT);
+const client = prismic.createClient(API_ENDPOINT);
 
 export async function getAllPosts() {
   return client.getAllByType("post");
